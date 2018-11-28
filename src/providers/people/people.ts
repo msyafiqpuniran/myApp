98959276ccedadeb23fc46b960702d8a2eb96d56
msyafiqpuniran <syafiq.puniran@gmail.com>
2018-11-28 10:07:47 +0800

    fourth commit

--- src/providers/people/people.ts
+++ src/providers/people/people.ts
@@ -9,6 +9,7 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class PeopleProvider {
+  private apigender = "";
   private apiUrl = "https://randomuser.me/api/?results=20";
   constructor(public http: HttpClient) {
     console.log('Hello PeopleProvider Provider');
@@ -16,6 +17,13 @@ export class PeopleProvider {
 
   getPeopleFromApi() {
     return this.http.get(this.apiUrl)
+  }
+
+  getpeopleFilterGender(genderType : String,page : number) {
+    this.apigender = "https://randomuser.me/api/?page=" + page + "&gender=" + genderType + "&results=20"
+    console.log(this.apigender)
+    return this.http.get(this.apigender)
+      
   }
 
 }
